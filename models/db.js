5c15577e644d3e1532c94527a63a42602afe7d18
doong-jo <sdong001@gmail.com>
2019-10-16 23:37:50 +0900

    refactor: db option 추가
    timezone, encoding 설정

--- models/db.js
+++ models/db.js
@@ -43,7 +43,14 @@ function getSequelizeInstance() {
         {
             host: process.env.DB_HOST,
             dialect: "mysql",
-            logging: false
+            logging: false,
+            timezone: "+09:00",
+            define: {
+                charset: "utf8mb4",
+                dialectOptions: {
+                    collate: "utf8mb4_general_ci"
+                }
+            }
         }
     );
 
