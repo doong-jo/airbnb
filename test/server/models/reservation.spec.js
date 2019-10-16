e13d34c6c61f16dd1a08a85b71ff41ad4b746306
doong-jo <sdong001@gmail.com>
2019-10-16 23:41:37 +0900

    test: 예약 테스트 작성

---
+++ test/server/models/reservation.spec.js
@@ -0,0 +1,40 @@
+import db from "../../../models/db";
+import Sequelize from "sequelize";
+
+describe("Model - Reservation", () => {
+    const { between, or } = Sequelize.Op;
+    const { reservation } = db;
+
+    afterAll(async done => {
+        db.sequelize.close();
+        done();
+    });
+
+    test("검색하는 체크인/체크아웃에 예약된 숙소를 조회한다.", async () => {
+        // given : 데이터는 Test DB에 이미 등록되어 있음
+        const exclude = ["id"];
+        const userCheckIn = new Date("2016-10-01 00:00:01 GMT+0900");
+        const userCheckOut = new Date("2016-10-03 00:00:01 GMT+0900");
+
+        console.log(userCheckIn);
+        console.log(userCheckOut);
+        // when
+        const responseData = await reservation.findAll({
+            attributes: { exclude },
+            // where: { check_in: { [between]: [userCheckIn, userCheckOut] } },
+            where: {
+                [or]: {
+                    check_in: {
+                        [between]: [userCheckIn, userCheckOut]
+                    },
+                    check_out: {
+                        [between]: [userCheckIn, userCheckOut]
+                    }
+                }
+            }
+        });
+
+        // then
+        expect(responseData.length).toBeTruthy();
+    });
+});
