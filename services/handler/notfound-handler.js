d3e5ef91375e7d23dccfa4eb22daeed29dfdf5b2
doong-jo <sdong001@gmail.com>
2019-10-17 17:20:26 +0900

    refactor: es6 모듈로 수정

--- services/handler/notfound-handler.js
+++ services/handler/notfound-handler.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const httpStatus = require("http-status");
 
-module.exports = (req, res, next) => {
+export default function(req, res, next) {
     if (res.statusCode === httpStatus.NOT_FOUND) {
         res.sendFile(path.join(__dirname, "notfound.html"));
         return;
     }
 
     next();
-};
+}
--- services/handler/serverinternal-handler.js
+++ services/handler/serverinternal-handler.js
@@ -1,7 +1,7 @@
-module.exports = (err, req, res) => {
+export default function(err, req, res) {
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
 
     res.status(err.status || 500);
     res.render("error");
-};
+}
