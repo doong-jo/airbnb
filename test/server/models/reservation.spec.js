0bdb3f36ad18b8d169b550e600e1f0e8bd00d0c7
doong-jo <sdong001@gmail.com>
2019-10-17 18:42:11 +0900

    refactor: 불필요한 날짜 값 수정
    between이 gte로 동작하므로 불필요하게되어 수정함

--- test/server/models/reservation.spec.js
+++ test/server/models/reservation.spec.js
@@ -13,11 +13,9 @@ describe("Model - Reservation", () => {
     test("검색하는 체크인/체크아웃에 예약된 숙소를 조회한다.", async () => {
         // given : 데이터는 Test DB에 이미 등록되어 있음
         const exclude = ["id"];
-        const userCheckIn = new Date("2016-10-01 00:00:01 GMT+0900");
-        const userCheckOut = new Date("2016-10-03 00:00:01 GMT+0900");
+        const userCheckIn = new Date("2016-10-01 00:00:00 GMT+0900");
+        const userCheckOut = new Date("2016-10-03 00:00:00 GMT+0900");
 
-        console.log(userCheckIn);
-        console.log(userCheckOut);
         // when
         const responseData = await reservation.findAll({
             attributes: { exclude },
