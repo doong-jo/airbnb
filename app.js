e997c835e3deb5573c5c433817a17b2addeab2b7
doong-jo <sdong001@gmail.com>
2019-10-22 14:10:02 +0900

    refactor: rename module path

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 import express from "express";
 import path from "path";
 import logger from "morgan";
-import notFoundHandler from "./services/handler/notfound-handler";
-import serverInternalHandler from "./services/handler/serverinternal-handler";
+import notFoundHandler from "./middleware/handlers/notfound-handler";
+import serverInternalHandler from "./middleware/handlers/serverinternal-handler";
 import mainRouter from "./routes/index";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -18,7 +18,7 @@ const { NODE_ENV, ENV_DEV, ENV_PROD } = process.env;
 if (!NODE_ENV || NODE_ENV === ENV_DEV) {
     process.env.NODE_ENV = ENV_DEV;
     console.log("--- DEVELOPMENT MODE ---");
-    const { initDatabaseSync } = require("./sql/db-initializer");
+    const { initDatabaseSync } = require("./db/db-initializer");
     initDatabaseSync();
     app.use(logger("dev"));
 } else if (process.env.NODE_ENV === ENV_PROD) {
