bc5041f1da374c8e9672d77bb6bdfba53da4742d
doong-jo <sdong001@gmail.com>
2019-10-23 23:10:23 +0900

    feat: storybook option과 외부 모듈 initilalize

--- client/storybook-playground/.storybook/config.js
+++ client/storybook-playground/.storybook/config.js
@@ -1,4 +1,21 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
+import { themes } from "@storybook/theming";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "react-dates/lib/css/_datepicker.css";
+import "react-dates/initialize";
+
+addParameters({
+    options: {
+        theme: themes.light,
+        panelPosition: "bottom",
+        sidebarAnimations: true,
+        showPanel: false,
+        hierarchySeparator: /\/|\./,
+        hierarchyRootSeparator: /\|/
+    }
+});
 // automatically import all files ending in *.stories.js
 configure(require.context("../src/stories", true, /\.stories\.js$/), module);
