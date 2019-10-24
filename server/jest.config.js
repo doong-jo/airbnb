4382054636894189ff3bceed1208422aee136ed3
doong-jo <sdong001@gmail.com>
2019-10-25 08:33:34 +0900

    refactor: remove puppeteer

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    preset: "jest-puppeteer",
     testRegex: "./*\\.spec\\.js$",
     testEnvironment: "node",
-    setupFilesAfterEnv: ["./test/setup.js", "expect-puppeteer"]
+    setupFilesAfterEnv: ["./test/setup.js"]
 };
