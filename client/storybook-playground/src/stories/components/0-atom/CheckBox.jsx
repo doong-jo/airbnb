5bd5e3d88c11251659ee3690baf3cfac2705f7c6
doong-jo <sdong001@gmail.com>
2019-10-27 18:46:48 +0900

    feat: checkBox 컴포넌트 작성

--- client/storybook-playground/src/stories/components/0-atom/CheckBox.jsx
+++ client/storybook-playground/src/stories/components/0-atom/CheckBox.jsx
@@ -1,25 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import { action } from "@storybook/addon-actions";
 
-function CheckBox(props) {
-    const CheckBox = styled.input`
-        type: checkbox;
+// Hide checkbox visually but remain accessible to screen readers.
+// Source: https://polished.js.org/docs/#hidevisually
+
+function CustomCheckBox(props) {
+    const Icon = styled.svg`
+        fill: none;
+        stroke: white;
+        stroke-width: 2px;
     `;
 
+    const CheckboxContainer = styled.div`
+        display: inline-block;
+        vertical-align: middle;
+    `;
+
+    const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
+        border: 0;
+        clip: rect(0 0 0 0);
+        clip-path: inset(50%);
+        height: 1px;
+        margin: -1px;
+        overflow: hidden;
+        padding: 0;
+        position: absolute;
+        white-space: nowrap;
+        width: 1px;
+    `;
+
+    const StyledCheckbox = styled.div`
+        display: inline-block;
+        width: 16px;
+        height: 16px;
+        background: ${props => (props.checked ? "#008488" : "white")};
+        border-radius: 3px;
+        transition: all 150ms;
+
+        ${HiddenCheckbox}:focus + & {
+            box-shadow: 0 0 0 3px gray;
+        }
+
+        ${Icon} {
+            visibility: ${props => (props.checked ? "visible" : "hidden")};
+        }
+    `;
+
+    const [checked, setChecked] = useState(true);
+
+    function handleCheckboxChange(event) {
+        action("hi");
+        setChecked({ checked: event.target.checked });
+    }
+
     return (
-        <>
-            <CheckBox />
-        </>
+        <CheckboxContainer>
+            <HiddenCheckbox
+                checked={checked}
+                {...props}
+                onChange={handleCheckboxChange}
+            />
+            <StyledCheckbox checked={checked}>
+                <Icon viewBox="0 0 24 24">
+                    <polyline points="20 6 9 17 4 12" />
+                </Icon>
+            </StyledCheckbox>
+        </CheckboxContainer>
     );
 }
 
-CheckBox.propTypes = {
-    isCheck: PropTypes.bool
+CustomCheckBox.propTypes = {
+    isCheck: PropTypes.bool,
+    checked: PropTypes.bool,
+    toggle: PropTypes.func
 };
 
-CheckBox.defaultProps = {
+CustomCheckBox.defaultProps = {
     isCheck: false
 };
 
-export default CheckBox;
+export default CustomCheckBox;
