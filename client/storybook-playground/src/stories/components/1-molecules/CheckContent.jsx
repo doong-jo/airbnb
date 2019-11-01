e01de1e154e9f111f10cca8909d0d4c2f666a39c
doong-jo <sdong001@gmail.com>
2019-11-02 00:40:06 +0900

    feat: checkContent 컴포넌트 작성

--- client/storybook-playground/src/stories/components/1-molecules/CheckContent.jsx
+++ client/storybook-playground/src/stories/components/1-molecules/CheckContent.jsx
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import CustomCheckBox from "../0-atom/CheckBox";
+import PropTypes from "prop-types";
 
 const Container = styled.div`
-    display: flex;
-    flex-direction: row;
+    display: row;
+    flex-direction: column;
     font-size: 1rem;
+    padding: 1rem;
 `;
 
+const ItemContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+const ContentContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const CheckItemTitle = styled.div`
+    font-weight: bold;
+`;
+
+const CheckBoxStyle = {
+    marginTop: "0.25rem",
+    marginRight: "0.5rem",
+    "user-select": "none"
+};
+
 function CheckContent(props) {
-    return (
-        <>
-            <Container>
-                <input type="checkbox" />
-                <div>
-                    <div>
-                        <b>집 전체</b>
-                    </div>
-                    <div>집 전체를 단독으로 사용합니다</div>
-                </div>
-            </Container>
-        </>
+    const { data } = props;
+    const [checks, setChecks] = useState(
+        props.data.map(() => {
+            return false;
+        })
     );
+
+    function getCheckItems() {
+        return data.map((v, i) => {
+            const { content, title, onChange } = v;
+
+            return (
+                <Container key={i}>
+                    <ItemContainer>
+                        <CustomCheckBox
+                            style={CheckBoxStyle}
+                            isChecked={checks[i]}
+                            onChange={e => {
+                                onChange(e);
+                                setChecks(
+                                    checks.map((v, ind) => {
+                                        return i === ind ? !v : v;
+                                    })
+                                );
+                            }}
+                        />
+                        <ContentContainer>
+                            <CheckItemTitle>{title}</CheckItemTitle>
+                            {content}
+                        </ContentContainer>
+                    </ItemContainer>
+                </Container>
+            );
+        });
+    }
+
+    return <>{getCheckItems()}</>;
 }
 
-CheckContent.propTypes = {};
+CheckContent.propTypes = {
+    /** [{title, content}, ...] */
+    data: PropTypes.array.isRequired
+};
 
+CheckContent.defaultProps = {
+    data: []
+};
 export default CheckContent;
