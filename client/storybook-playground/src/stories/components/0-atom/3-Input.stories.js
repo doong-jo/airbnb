522d35a9f6cad466ca6e45d4909534cbca42b220
doong-jo <sdong001@gmail.com>
2019-11-02 00:37:30 +0900

    refactor: checkBox 컴포넌트 수정
    부모에게 prop를 받아 동작하도록 수정하고, 불필요한 element 제거

--- client/storybook-playground/src/stories/components/0-atom/3-Input.stories.js
+++ client/storybook-playground/src/stories/components/0-atom/3-Input.stories.js
@@ -1,8 +1,24 @@
 import React from "react";
 import CustomCheckBox from "./CheckBox";
+import ToggleSwitch from "./ToggleSwitch";
+import { action } from "@storybook/addon-actions";
+import { boolean } from "@storybook/addon-knobs";
 
 export default {
     title: "Components|Atom/Input"
 };
 
-export const checkBox = () => <CustomCheckBox />;
+export const checkBox = () => {
+    function onChange(e) {
+        action("checked")(e);
+    }
+
+    return (
+        <CustomCheckBox
+            isChecked={boolean("checked", false)}
+            onChange={e => {
+                onChange(e);
+            }}
+        />
+    );
+};
--- client/storybook-playground/src/stories/components/0-atom/CheckBox.jsx
+++ client/storybook-playground/src/stories/components/0-atom/CheckBox.jsx
@@ -1,84 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import { action } from "@storybook/addon-actions";
-
-// Hide checkbox visually but remain accessible to screen readers.
-// Source: https://polished.js.org/docs/#hidevisually
 
 function CustomCheckBox(props) {
+    const { isChecked, onChange, style } = props;
+
     const Icon = styled.svg`
         fill: none;
         stroke: white;
         stroke-width: 2px;
-    `;
-
-    const CheckboxContainer = styled.div`
-        display: inline-block;
-        vertical-align: middle;
-    `;
-
-    const HiddenCheckbox = styled.input.attrs({ type: "checkbox" })`
-        border: 0;
-        clip: rect(0 0 0 0);
-        clip-path: inset(50%);
-        height: 1px;
-        margin: -1px;
-        overflow: hidden;
-        padding: 0;
-        position: absolute;
-        white-space: nowrap;
-        width: 1px;
+        opacity: ${isChecked ? 1 : 0};
     `;
 
     const StyledCheckbox = styled.div`
         display: inline-block;
+        vertical-align: middle;
+        line-height: 0;
         width: 16px;
         height: 16px;
-        background: ${props => (props.checked ? "#008488" : "white")};
+        background: ${isChecked ? "#008488" : "#fff"};
+        border: ${isChecked ? 0 : "1px solid #ccc"};
         border-radius: 3px;
-        transition: all 150ms;
-
-        ${HiddenCheckbox}:focus + & {
-            box-shadow: 0 0 0 3px gray;
-        }
-
-        ${Icon} {
-            visibility: ${props => (props.checked ? "visible" : "hidden")};
-        }
     `;
 
-    const [checked, setChecked] = useState(true);
-
-    function handleCheckboxChange(event) {
-        action("hi");
-        setChecked({ checked: event.target.checked });
-    }
-
     return (
-        <CheckboxContainer>
-            <HiddenCheckbox
-                checked={checked}
-                {...props}
-                onChange={handleCheckboxChange}
-            />
-            <StyledCheckbox checked={checked}>
-                <Icon viewBox="0 0 24 24">
-                    <polyline points="20 6 9 17 4 12" />
-                </Icon>
-            </StyledCheckbox>
-        </CheckboxContainer>
+        <StyledCheckbox
+            style={style}
+            onClick={e => {
+                onChange(e);
+            }}
+        >
+            <Icon viewBox="0 0 24 24">
+                <polyline points="20 6 9 17 4 12" />
+            </Icon>
+        </StyledCheckbox>
     );
 }
 
 CustomCheckBox.propTypes = {
-    isCheck: PropTypes.bool,
-    checked: PropTypes.bool,
-    toggle: PropTypes.func
+    isChecked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func,
+    style: PropTypes.object
 };
 
 CustomCheckBox.defaultProps = {
-    isCheck: false
+    onChange: e => {},
+    isChecked: false
 };
 
 export default CustomCheckBox;
