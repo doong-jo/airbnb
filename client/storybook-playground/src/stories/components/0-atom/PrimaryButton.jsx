dbab83485005d8db783b684933c12ea040b5fd24
doong-jo <sdong001@gmail.com>
2020-08-02 14:09:25 +0900

    feat: test modify

--- client/storybook-playground/src/stories/components/0-atom/PrimaryButton.jsx
+++ client/storybook-playground/src/stories/components/0-atom/PrimaryButton.jsx
@@ -1,54 +1,55 @@
-import React from "react";
-import { Button } from "reactstrap";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 const PrimaryButtonStyle = styled.div`
-    display: inline-block;
+  display: inline-block;
 
-    & button {
-        background-color: #ff595f;
-        border-color: #ff595f;
-        min-width: 5rem;
-    }
+  & button {
+    background-color: #ff595e;
+    border-color: #ff595e;
+    min-width: 5rem;
+    border-radius: 24px;
+  }
 
-    & button:focus {
-        box-shadow: 0 0 0 0.2rem rgba(199, 55, 55, 0.5);
-    }
+  & button:focus {
+    box-shadow: 0 0 0 0.2rem rgba(199, 55, 55, 0.5);
+  }
 
-    & button:active {
-        background-color: #ff595f !important;
-        border-color: #ff595f !important;
-        box-shadow: 0 0 0 0.2rem rgba(199, 55, 55, 0.5) !important;
-    }
+  & button:active {
+    background-color: #ff595f !important;
+    border-color: #ff595f !important;
+    box-shadow: 0 0 0 0.2rem rgba(199, 55, 55, 0.5) !important;
+  }
 
-    & button:hover {
-        color: #fff;
-        background-color: rgba(199, 55, 55, 1);
-        border-color: #ff595f;
-    }
+  & button:hover {
+    color: #fff;
+    background-color: rgba(199, 55, 55, 1);
+    border-color: #ff595f;
+  }
 `;
 
 function PrimaryButton(props) {
-    return (
-        <>
-            <PrimaryButtonStyle>
-                <Button
-                    color="primary"
-                    onClick={() => {
-                        props.onClick();
-                    }}
-                >
-                    {props.content}
-                </Button>
-            </PrimaryButtonStyle>
-        </>
-    );
+  return (
+    <>
+      <PrimaryButtonStyle>
+        <Button
+          color="primary"
+          onClick={() => {
+            props.onClick();
+          }}
+        >
+          {props.content}
+        </Button>
+      </PrimaryButtonStyle>
+    </>
+  );
 }
 
 PrimaryButton.propTypes = {
-    content: PropTypes.string.isRequired,
-    onClick: PropTypes.func.isRequired
+  content: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default PrimaryButton;
